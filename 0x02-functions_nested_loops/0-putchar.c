d81e4e0a90c791abb7138d98ea7e80efc4cfb016
Jackson-Muendo <jackmwexh1225@gmail.com>
2022-03-15 11:03:18 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,36 +1,29 @@
 #include "main.h"
 
 /**
-
- * print_alphabet - print alphabet a-z afer looping
-
- *
-
+ * main - Entry point
+ * prints "_putchar" followed by a new line
+ * the program prints "_putchar
  * Return: 0
-
  */
 
 
 
-void print_alphabet(void)
+int main(void)
 
 {
 
+	char ch = "_putchar";
 
-
-	char i;
-
-
-
-	i = 'a';
+	int i = 0;
 
 
 
-	while (i <= 'z')
+	while (ch != '\0')
 
 	{
 
-		_putchar(i);
+		_putchar(ch);
 
 		i++;
 
@@ -39,5 +32,7 @@ void print_alphabet(void)
 	_putchar('\n');
 
 
+
+	return (0);
 
 }
