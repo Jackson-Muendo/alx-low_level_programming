0d4d40b21ab13203cf91bfae27975c0100570a46
Jackson-Muendo <jackmwexh1225@gmail.com>
2022-03-15 09:23:26 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -23,53 +23,30 @@ void jack_bauer(void)
 	{
 
 		_putchar(a + '0');
-
 		_putchar(b + '0');
-
 		_putchar(':');
-
 		_putchar(c + '0');
-
 		_putchar(d + '0');
-
 		_putchar('\n');
-
-
-
 		d++;
-
 		if (d > 9)
-
 		{
-
 			d = 0;
-
 			c++;
-
 		}
-
 		if (c > 5)
-
 		{
-
 			c = 0;
-
 			b++;
-
 		}
 
 		if (b > 9)
 
 		{
-
 			b = 0;
-
 			a++;
-
 		}
 
 		x++;
-
 	}
-
 }
