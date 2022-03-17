996daa0bcf77ae84184bb3fd4dabe0fcb569593d
Jackson-Muendo <jackmwexh1225@gmail.com>
2022-03-16 20:51:48 -0700

    3-convert_day.c

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -1,20 +1,20 @@
-nclude "main.h"
+#include "main.h"
 
 
 
 /**
- *
- * * convert_day - converts day of month to day of year, without accounting
- *
- * * for leap year
- *
- * * @month: month in number format
- *
- * * @day: day of month
- *
- * * Return: day of year
- *
- * */
+
+* convert_day - converts day of month to day of year, without accounting
+
+* for leap year
+
+* @month: month in number format
+
+* @day: day of month
+
+* Return: day of year
+
+*/
 
 
 
@@ -22,78 +22,163 @@ int convert_day(int month, int day)
 
 {
 
-		switch (month)
+    switch (month)
 
-				{
+    {
 
-						case 2:
+        case 2:
 
-									day = 31 + day;
+            day = 31 + day;
 
-											break;
+            break;
 
-												case 3:
+        case 3:
 
-													day = 59 + day;
+            day = 59 + day;
 
-															break;
+            break;
 
-																case 4:
+        case 4:
 
-																	day = 90 + day;
+            day = 90 + day;
 
-																			break;
+            break;
 
-																				case 5:
+        case 5:
 
-																					day = 120 + day;
+            day = 120 + day;
 
-																							break;
+            break;
 
-																								case 6:
+        case 6:
 
-																									day = 151 + day;
+            day = 151 + day;
 
-																											break;
+            break;
 
-																												case 7:
+        case 7:
 
-																													day = 181 + day;
+            day = 181 + day;
 
-																															break;
+            break;
 
-																																case 8:
+        case 8:
 
-																																	day = 212 + day;
+            day = 212 + day;
 
-																																			break;
+            break;
 
-																																				case 9:
+        case 9:
 
-																																					day = 243 + day;
+            day = 243 + day;
 
-																																							break;
+            break;
 
-																																								case 10:
+        case 10:
 
-																																									day = 273 + day;
+            day = 273 + day;
 
-																																											break;
+            break;
 
-																																												case 11:
+        case 11:
 
-																																													day = 304 + day;
+            day = 304 + day;
 
-																																															break;
+            break;
 
-																																																case 12:
+        case 12:
 
-																																																	day = 334 + day;
+            day = 334 + day;
 
-																																																			break;
+            break;
 
-			default:
-				break;
-				}
-			return (day);
+        default:
+
+            break;
+
+    }
+
+    return (day);
+
 }
+
+
+
+carrie@ubuntu:/debugging$
+
+carrie@ubuntu:/debugging$ cat 3-print_remaining_days.c
+
+#include <stdio.h>
+
+#include "main.h"
+
+
+
+/**
+
+* print_remaining_days - takes a date and prints how many days are
+
+* left in the year, taking leap years into account
+
+* @month: month in number format
+
+* @day: day of month
+
+* @year: year
+
+* Return: void
+
+*/
+
+
+
+void print_remaining_days(int month, int day, int year)
+
+{
+
+    if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+
+    {
+
+        if (month >= 2 && day >= 60)
+
+        {
+
+            day++;
+
+        }
+
+
+
+        printf("Day of the year: %d\n", day);
+
+        printf("Remaining days: %d\n", 366 - day);
+
+    }
+
+    else
+
+    {
+
+        if (month == 2 && day == 60)
+
+        {
+
+            printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+
+        }
+
+        else
+
+        {
+
+            printf("Day of the year: %d\n", day);
+
+            printf("Remaining days: %d\n", 365 - day);
+
+        }
+
+    }
+
+}
+
