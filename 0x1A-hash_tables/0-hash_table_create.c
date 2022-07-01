db9d6068cbd8c65ee98da7fe1c76a2619121efe8
Jackson-Muendo <jackmwexh1225@gmail.com>
2022-06-30 21:09:35 -0700

    ts

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,25 +1,65 @@
 #include "hash_tables.h"
 
+
+
 /**
+
  * hash_table_create - creates a hash table
- * @size: size of the hash table
- * Return: pointer to new hash table else NULL
+
+ * @size: size of the array
+
+ *
+
+ * Return: pointer to hash table, otherwise return NULL
+
  */
 
-hash_table_t *hash_table_create(unsigned long int size)
-{
-	hash_table_t *table = calloc(1, sizeof(hash_table_t));
 
-	if (!size)
-		return (NULL);
-	if (!table)
-		return (NULL);
-	table->size = size;
-	table->array = calloc(size, sizeof(hash_node_t **));
-	if (!table->array)
-	{
-		free(table);
-		return (NULL);
-	}
-	return (table);
+
+hash_table_t *hash_table_create(unsigned long int size)
+
+{
+
+hash_table_t *ht;
+
+unsigned long int index;
+
+
+
+if (size < 1)
+
+return (NULL);
+
+
+
+ht = malloc(sizeof(hash_table_t));
+
+if (ht == NULL)
+
+return (NULL);
+
+ht->array = malloc(sizeof(hash_node_t *) * size);
+
+if (ht->array == NULL)
+
+{
+
+free(ht);
+
+return (NULL);
+
+}
+
+
+
+ht->size = size;
+
+
+
+for (index = 0; index < size; index++)
+
+ht->array[index] = NULL;
+
+return (ht);
+
 }
--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,33 +1,69 @@
 #include "hash_tables.h"
 
+
+
 /**
- * hash_table_delete - deletes hash table ht
- * @ht: hash table to delete
+
+ * hash_table_delete - deletes hash table
+
+ * @ht: hash table
+
  */
 
-void hash_table_delete(hash_table_t *ht)
-{
-	register unsigned int i = 0;
-	hash_node_t *current, *temp;
 
-	if (!ht)
-		return;
-	for (i = 0; i < ht->size; i++)
-	{
-		if (ht->array[i])
-		{
-			current = ht->array[i];
-			while (current)
-			{
-				temp = current;
-				free(current->key);
-				if (current->value)
-					free(current->value);
-				current = current->next;
-				free(temp);
-			}
-		}
-	}
-	free(ht->array);
-	free(ht);
+
+void hash_table_delete(hash_table_t *ht)
+
+{
+
+unsigned long int index;
+
+hash_node_t *node, *next_node;
+
+
+
+if (ht == NULL)
+
+return;
+
+
+
+index = 0;
+
+while (index < ht->size)
+
+{
+
+node = ht->array[index];
+
+while (node != NULL)
+
+{
+
+next_node = node->next;
+
+free(node->key);
+
+free(node->value);
+
+free(node);
+
+if (next_node != NULL)
+
+node = next_node;
+
+else
+
+break;
+
+}
+
+index++;
+
+}
+
+free(ht->array);
+
+free(ht);
+
 }
